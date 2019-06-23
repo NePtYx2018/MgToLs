a7d78356f9ff7d4d34825f0e04b3daa40719706c
NePtYx2018 <52055199+NePtYx2018@users.noreply.github.com>
2019-06-22 20:56:33 -0400

    Add files via upload

---
+++ MgToLs.sh
@@ -0,0 +1,26 @@
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Lo sentimos por hacerte esperar,pero MgToLs se"
+echo "esta descomprimiendo..."
+unzip MgToLs.zip
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Gracias por descargar MgToLs pasate por"
+echo "mi canal ayi subo cosas interesantes:"
+echo "[https://youtu.be/UrOBPSU6u-8]"
+rm MgToLs.zip
+rm MgToLs.sh
